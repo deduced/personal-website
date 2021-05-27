e608ad9a3e086167e9d036af317cf20dae97c2d1
Charlie Astrada <carlosastrada@gmail.com>
2021-05-26 22:24:40 -0300

    Add Hero to index
    Hero contains a background image and the coming soon placeholder text. Currently, the image is not optimized.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import { Link, graphql, useStaticQuery } from "gatsby";
+
+const ImageBackground = styled("div")`
+  background-image: url("/static/hero.jpeg");
+  background-position: top 20% center;
+  background-size: cover;
+  height: 50vh;
+
+  + * {
+    margin-top: 0;
+  }
+`;
+
+const TextBox = styled("div")`
+  background-image: linear-gradient(to top, #ddbbffdd 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p {
+    color: #222;
+    margin-top: 0;
+  }
+
+  a {
+    color: #222;
+    margin-top: 0.5rem;
+  }
+`;
+
+interface HeroProps {}
+
+const Hero: React.FC<HeroProps> = ({}) => {
+  return (
+    <ImageBackground>
+      <TextBox>
+        <h1>Coming Soon!</h1>
+        <p>
+          A blog about modern full stack web development, entrepreneurship, and
+          some data science sprinkled in.
+        </p>
+      </TextBox>
+    </ImageBackground>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,21 @@ import * as React from "react";
 import { PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import usePosts, { Post } from "../hooks/use-posts";
-import { PostResult } from "../hooks/use-posts";
 import PostPreview from "../components/PostPreview";
+import Hero from "../components/Hero";
 
-// markup
 const IndexPage: React.FC<PageProps> = () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Coming Soon!</h1>
-      <p>
-        A blog about modern full stack web development, entrepreneurship, and
-        some data science sprinkled in.
-      </p>
-
-      <h2>Read My Blog</h2>
-      {posts.map((post: Post) => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read My Blog</h2>
+        {posts.map((post: Post) => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
 
